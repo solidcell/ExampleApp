5aa08dfac8b044b0546c5f0a3975717c29df5929
Jinoo Baek <baek32jinoo@gmail.com>
2015-11-23 19:43:52 -0800

    Add comments
    Comments around why the application isn't passed in the forward calls

--- ExampleApp/AppDelegate.swift
+++ ExampleApp/AppDelegate.swift
@@ -6,9 +6,11 @@ import Swinject
  
  It creates the appropriate dependency injection bindings,
  creates the AppProxy and forwards calls to it, and
- sets the window.
+ sets the window. The application object isn't passed in the 
+ forward calls as it should be injected into the appProxy,
+ which allows us to fake the application object
  
- It is not part of the integration tests. Instead, the tests
+ This class is not part of the integration tests. Instead, the tests
  will play much the same role from this perspective. This allows
  you to start and control the app in the same manner as iOS does.
 ***********************/
