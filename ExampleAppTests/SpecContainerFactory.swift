d754608324104d45a37347b14ffd9c3b8380dce4
Jinoo Baek <baek32jinoo@gmail.com>
2015-11-23 21:01:10 -0800

    Edit comments

--- ExampleAppTests/SpecContainerFactory.swift
+++ ExampleAppTests/SpecContainerFactory.swift
@@ -4,7 +4,6 @@ import Swinject
 
 // This is the configuration for the specs
 class SpecContainerFactory {
-
     static func createContainer() -> Container {
         return ContainerFactory.registerServices(
             screen: { FakeScreen() },
